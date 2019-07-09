4bc36654d1ca01a049355ba540a7d00697711761
Daniel Weibel <danielmweibel@gmail.com>
2019-07-09 16:06:32 +0200

    Log request handling

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ const port = 8080
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
+	log.Printf("Handling request from %v\n", r.RemoteAddr)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Printf("> App listening on http://localhost:%v\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	if err != nil {
+		panic(err)
+	}
 }
